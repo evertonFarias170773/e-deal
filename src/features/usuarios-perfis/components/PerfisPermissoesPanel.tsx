b7aa42d3043afce47a50f24b1ff6a244eb5b3b87
Everton Farias <dseg.dev@gmail.com>
2026-07-01 21:40:16 -0300

    style(usuarios-perfis): reposiciona FAB de salvar para evitar sobreposição com o Chat Interno

--- src/features/usuarios-perfis/components/PerfisPermissoesPanel.tsx
+++ src/features/usuarios-perfis/components/PerfisPermissoesPanel.tsx
@@ -560,7 +560,7 @@ export function PerfisPermissoesPanel() {
       )}
       {/* Botão Salvar Flutuante (Sticky) */}
       {perfilSelecionado && perfilSelecionado.slug !== "super_admin" && (
-        <div className="fixed bottom-6 right-6 z-40">
+        <div className="fixed bottom-24 right-6 md:right-8 z-40">
           <button
             type="button"
             onClick={handleOpenConfirmacao}
